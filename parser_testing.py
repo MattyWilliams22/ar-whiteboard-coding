fc070cb00ad9af0c0e49bd201842d9dbe8ae6e14
Matty Williams <71126779+MattyWilliams22@users.noreply.github.com>
2025-02-26 18:00:29 +0000

    Desktop app to show and run parsed code with error highlighting

--- parser_testing.py
+++ parser_testing.py
@@ -1,7 +1,9 @@
 from code_detection.parse_code import *
+from output.app import *
 
 def main():
-    test_parsing()
+    python_code = test_parsing()
+    run_editor(python_code)
 
 if __name__ == "__main__":
     main()
