bd2e6ed9e470724d291a212e902126d8bb1e3999
Matty Williams <71126779+MattyWilliams22@users.noreply.github.com>
2025-02-26 19:08:25 +0000

    App to show projected overlay

--- parser_testing.py
+++ parser_testing.py
@@ -1,9 +1,9 @@
 from code_detection.parse_code import *
-from output.app import *
+from output.projector_app import *
 
 def main():
     python_code = test_parsing()
-    run_editor(python_code)
+    run_editor(initial_code=python_code)
 
 if __name__ == "__main__":
     main()
