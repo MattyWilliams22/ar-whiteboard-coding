28da9725cdec08a138924e3e5782e160d296daff
Matty Williams <71126779+MattyWilliams22@users.noreply.github.com>
2025-03-08 16:44:10 +0000

    Added extra marker-keyword mappings

--- code_detection/markers/keywords.py
+++ code_detection/markers/keywords.py
@@ -20,6 +20,32 @@ def get_keyword(index):
         case 8:
             return "Return"
         case 9:
-            return "Break"
+            return "Else If"
+        case 10:
+            return "End"
+        case 11:
+            return "Take"
+        case 12:
+            return "Call"
+        case 13:
+            return "With"
+        case 14:
+            return "From"
+        case 15:
+            return "To"
+        case 16:
+            return "Str"
+        case 44:
+            return "Python Code:"
+        case 45:
+            return "Output:"
+        case 46:
+            return "Bottom Left"
+        case 47:
+            return "Bottom Right"
+        case 48:
+            return "Top Right"
+        case 49:
+            return "Top Left"
         case _:
             return "UNKNOWN"
